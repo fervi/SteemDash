265ed13cfa9c5e45d864b5b6bb8cdf9545c208a9
fervi <thunderstormpolska@o2.pl>
2017-07-24 01:34:13 +0200

    Fixed lang_opensource bug

--- banpost.php
+++ banpost.php
@@ -1,11 +1,1 @@
 <?php
-$userban[0]="dobro88888888";
-$postban[0]="1500709336";
-$userban[1]="dobro88888888";
-$postban[1]="1500708835";
-$userban[2]="dobro88888888";
-$postban[2]="1500707673";
-$userban[3]="andrejcibik";
-$postban[3]="1500665469";
-$userban[4]="siol";
-$postban[4]="1500394871";
