9ef6247393796bfa845421c4d4aaa41376599f65
fervi <thunderstormpolska@o2.pl>
2017-07-24 01:57:04 +0200

    Added Language menu

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <?php
 include("config.php");
+$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
 
 <html ng-app="app"><head>
@@ -35,17 +36,41 @@ include("config.php");
     </div>
 
 
-
-
- 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
       <ul class="nav navbar-nav">
 <li><a href="core.php?type=created"><?php echo $lang_created; ?></a></li>
 <li><a href="core.php?type=hot"><?php echo $lang_hot; ?></a></li>
 <li><a href="core.php?type=trending"><?php echo $lang_trending; ?></a></li>
       </ul>
+
       <ul class="nav navbar-nav navbar-right">
         <li><a href="https://busy.org/transfer?to=fervi&amount=1.000&currency=SBD&memo=Donate%20for%20Fervi"><?php echo $lang_donate; ?></a></li>
+
+
+       <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">LANGs<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+<?php
+
+if (strpos($actual_link, 'www') !== false) {
+echo '<li><a href="'.str_replace("www", "pl", $actual_link).'">Polish</a></li>';
+echo '<li><a href="'.str_replace("www", "fr", $actual_link).'">French</a></li>';
+}
+
+if (strpos($actual_link, 'pl') !== false) {
+echo '<li><a href="'.str_replace("pl", "www", $actual_link).'">English</a></li>';
+echo '<li><a href="'.str_replace("pl", "fr", $actual_link).'">French</a></li>';
+}
+
+if (strpos($actual_link, 'fr') !== false) {
+echo '<li><a href="'.str_replace("fr", "www", $actual_link).'">English</a></li>';
+echo '<li><a href="'.str_replace("fr", "pl", $actual_link).'">Polish</a></li>';
+}
+
+?>
+          </ul>
+</li>
+
         <li><a href="newpost.php" ng-show="main.isAuth()"><?php echo $lang_add_dash; ?></a></li>
         <li><a href="https://steemconnect.com/" ng-show="main.isAuth()">@{{username}}</a></li>
         <li><a ng-show="main.isAuth()" class="btn btn-secondary" href="https://steemconnect.com/logout"><?php echo $lang_logout; ?></a></li>
